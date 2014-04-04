acb0ca5b17c3459f993f488f9fc563e9c37a40dd
nhiirage <nhiirage@gmail.com>
2014-04-05 06:48:17 +0800

    no message

--- ah/handlers/admin_handler.py
+++ ah/handlers/admin_handler.py
@@ -4,7 +4,4 @@ from google.appengine.ext.webapp import blobstore_handlers
 
 class AdminHandler(session.SessionRequestHandler):
     def get(self, **routes):
-        if not self.session.get('is_admin'):
-            self.redirect('/ah/auth/')
-        else:
-            self.response.out.write(self.session.get('is_admin', 'Fuck!'))
+        self.response.out.write("Inside Admin")
